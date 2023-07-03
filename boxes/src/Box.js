73af2e5bcc7a00a3b4e8bbd339fdf5885a097e10
Christopher Hounnou <126211266+chris-hounnou@users.noreply.github.com>
2023-07-02 22:15:51 -0500

    changes

---
+++ boxes/src/Box.js
@@ -0,0 +1,28 @@
+
+
+import React from "react"
+
+
+function Box({
+    id,
+    handleRemove,
+    width = 5,
+    height = 5,
+    backgroundColor = "blue"
+}) {
+    const remove = () => handleRemove(id);
+    return (
+        <div>
+            <div
+                style={{
+                    height: `${height}em`,
+                    width: `${width}em`,
+                }}
+            />
+            <button onClick={remove}>Remove The Box!</button>
+        </div>
+    )
+}
+
+
+export default Box;
---
+++ todos/src/NewTodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { v4 as uuid } from 'uuid';
+
+
+function NewTodoForm({ createTodo }) {
+    const [task, setTask] = useState("");
+
+
+    const handleChange = evt => {
+        setTask(evt.target.value);
+    };
+
+
+    const gatherInput = evt => {
+        evt.preventDefault();
+        createTodo({ task, id: uuid() });
+        setTask("");
+    };
+    return (
+        <div>
+            <form onSubmit={gatherInput}>
+                <label html For="task">Task:</label>
+                <input
+                    id="task"
+                    name="task"
+                    type="text"
+                    onChange={handleChange}
+                    value={task} />
+                <button>Add A Task!</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default NewTodoForm;
